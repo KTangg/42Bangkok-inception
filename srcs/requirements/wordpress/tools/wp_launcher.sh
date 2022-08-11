42d4494ed7ae7b4d881e607861206798313545c3
KTangg <k.tangtangtang@gmail.com>
2022-08-11 16:12:13 +0700

    Fix typo

--- srcs/requirements/wordpress/tools/wp_launcher.sh
+++ srcs/requirements/wordpress/tools/wp_launcher.sh
@@ -9,7 +9,7 @@ echo "Connected to mariadb-server"
 if [ ! -f wp-config.php ]; then
 	wp core download --allow-root
 	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
-	wp core install --url=$WP_ADDRESS --title=$WP_TITLE --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL--skip-email --allow-root
+	wp core install --url=$WP_ADDRESS --title=$WP_TITLE --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 	chown -R www-data:www-data /var/www/inception
 fi
