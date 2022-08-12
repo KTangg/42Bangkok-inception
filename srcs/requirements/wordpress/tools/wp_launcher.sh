742bd9777a64a70716865851bf9423d805a8eef4
KTangg <k.tangtangtang@gmail.com>
2022-08-12 15:07:05 +0700

    FTP server add

--- srcs/requirements/wordpress/tools/wp_launcher.sh
+++ srcs/requirements/wordpress/tools/wp_launcher.sh
@@ -11,8 +11,9 @@ if [ ! -f wp-config.php ]; then
 	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
 	wp core install --url=$WP_ADDRESS --title=$WP_TITLE --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-	chown -R www-data:www-data /var/www/inception
 fi
+
+chown -R www-data:www-data /var/www/inception
 
 # Create socket directory
 if [ ! -d /run/php ]; then
